5875455b9446001f84421714101ec5bcae02fd50
nhhandevops <148748303+nhhandevops@users.noreply.github.com>
2025-03-12 18:55:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask 
-app = Flask(__name__)   #app instance
- 
+app = Flask(__name__) 
 
 #root of application 
-@app.route("/")       #app as a decorator to create each router or URL provided by application 
+@app.route("/")       
 def hello(): 
-    return "Hello, Welcome to Searce pvt ltd" 
-
+    return "Hello, Welcome to Antsomi" 
  
 if __name__ == "__main__": 
     app.run() 
