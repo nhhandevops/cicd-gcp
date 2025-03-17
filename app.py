a73481526c92bf67a10bf55c0b7ea657af1b2726
nhhandevops <nguyenhuuhoangan2504@gmail.com>
2025-03-17 10:11:37 +0700

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #root of application 
 @app.route("/")       
 def hello(): 
-    return "Hello Customer, Welcome to Antsomi<br>" 
+    return "Hello Customer, Welcome to Antsomi" 
  
 if __name__ == "__main__": 
     app.run() 
